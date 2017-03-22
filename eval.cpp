2bb3c787964322c40089ff0123bd932a3b604206
sam <sam@QUELANA>
2017-03-22 17:42:52 -0400

    add_expr unit test initial pass

--- eval.cpp
+++ eval.cpp
@@ -7,6 +7,9 @@ void eval_visitor::visit( bool_expr& e )
 
 void eval_visitor::visit( and_expr& e )
 {
+	int lhs = eval( e.get_e1() );
+	int rhs = eval( e.get_e2() );
+	r = lhs && rhs;
 }
 
 int eval( expr& e )
