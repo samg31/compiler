b8c21ccf4d17167b2e954888b45fc95c7edda445
samgoodrick <sdg31@zips.uakron.edu>
2017-04-10 22:00:24 -0400

    removed return in switch in next

--- lexer.cpp
+++ lexer.cpp
@@ -32,46 +32,26 @@ char lexer::lookahead()
 
 token* lexer::next()
 {
-	token* r;
+	token* r = nullptr;
 	buf.clear();
 	while( !eof() )
 	{
 		switch( lookahead() )
 		{
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-		{
-			do
-			{
-				consume();
-			} while( !eof() && std::isdigit( lookahead() ) );
-			int n = std::stoi( buf );
-			token* t = new int_token( n );
-			return t;
-		}
-		break;
 		case '(':
 			consume();
-			return new punc_token( lparen_tok );
+			r = new punc_token( lparen_tok );
 			break;
 		case ')':
 			consume();
-			return new punc_token( rparen_tok );
+			r = new punc_token( rparen_tok );
 			break;
 		case '&':
 			consume();
 			if( lookahead() == '&' )
 			{
 				consume();
-				return new punc_token( ampersand_tok );
+				r = new punc_token( ampersand_tok );
 			}
 			break;
 		case '|':
@@ -79,83 +59,83 @@ token* lexer::next()
 			if( lookahead() == '|' )
 			{
 				consume();
-				return new punc_token( bar_tok );
+				r = new punc_token( bar_tok );
 			}
 			break;
 		case '^':
 			consume();
-			return new punc_token( caret_tok );
+			r = new punc_token( caret_tok );
 			break;
 		case '!':
 			consume();
 			if( lookahead() == '=' )
 			{
 				consume();
-				return new punc_token( exclmeq_tok );
+				r = new punc_token( exclmeq_tok );
 			}
-			return new punc_token( exclm_tok );
+			r = new punc_token( exclm_tok );
 			break;
 		case '+':
 			consume();
-			return new punc_token( plus_tok );
+			r = new punc_token( plus_tok );
 			break;
 		case '-':
 			consume();
-			return new punc_token( minus_tok );
+			r = new punc_token( minus_tok );
 			break;
 		case '*':
 			consume();
-			return new punc_token( asterix_tok );
+			r = new punc_token( asterix_tok );
 			break;
 		case '/':
 			consume();
-			return new punc_token( slash_tok );
+			r = new punc_token( slash_tok );
 			break;
 		case '%':
 			consume();
-			return new punc_token( percent_tok );
+			r = new punc_token( percent_tok );
 			break;
 		case '?':
 			consume();
-			return new punc_token( question_tok );
+			r = new punc_token( question_tok );
 			break;
 		case ':':
 			consume();
-			return new punc_token( colon_tok );
+			r = new punc_token( colon_tok );
 			break;
 		case ';':
 			consume();
-			return new punc_token( semicolon_tok );
+			r = new punc_token( semicolon_tok );
 			break;
 		case '<':
 			consume();
 			if( lookahead() == '=' )
 			{
 				consume();
-				return new punc_token( lesseq_tok );
+				r = new punc_token( lesseq_tok );
 			}
 			else
-				return new punc_token( less_tok );
+				r = new punc_token( less_tok );
 			break;
 		case '>':
 			consume();
 			if( lookahead() == '=' )
 			{
 				consume();
-				return new punc_token( greatereq_tok );
+				r = new punc_token( greatereq_tok );
 			}
 			else
-				return new punc_token( greater_tok );
+				r = new punc_token( greater_tok );
 			break;
 		case '=':
 			consume();
 			if( lookahead() == '=' )
 			{
 				consume();
-				return new punc_token( equal_tok );
+				r = new punc_token( equal_tok );
 			}
 			else
-				return new punc_token( assign_tok );
+				r = new punc_token( assign_tok );
 			break;
 		case '#':
 			while( lookahead() != '\n' )
@@ -181,21 +161,30 @@ token* lexer::next()
 				{
 					if( iter->first == "true" ||
 						iter->first == "false" )
-						return new bool_token( iter->second );
+						r = new bool_token( iter->second );
 					else
-						return new punc_token( iter->second );
+						r = new punc_token( iter->second );
 				}
 				else
 				{
 					// add new term to symbol table
 					auto name = sym.insert( buf );
-					return new id_token( *name );
+					r = new id_token( *name );
 				}
+			}
+			else if( std::isdigit( lookahead() ) )
+			{
+				do
+				{
+					consume();
+				} while( !eof() && std::isdigit( lookahead() ) );
+				int n = std::stoi( buf );
+				token* t = new int_token( n );
+				r = t;
 			}
 
 			throw std::runtime_error( "unknown token encountered\n" );
-			return nullptr;
 		}
 	}
-	return nullptr;
+	return r;
 }
